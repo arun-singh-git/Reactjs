c9a405ad4a275d6c7f23f460f21db65f53778f73
arun singh <arunsingh@indiatvnews.com>
2020-06-08 20:34:40 +0530

    react routing code commit

---
+++ src/About.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './aboutstyle.css';
+
+const About = () => {
+
+		return (
+			<div class="aboutstyle">
+				<h2>This is About page.</h2>
+			</div>
+			)
+}
+
+export default About;
+
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './contactstyle.css';
+
+const Contact = () => {
+
+		return (
+			<div class="contactstyle">
+				<h2>This is Contact page.</h2>
+			</div>
+			)
+}
+
+export default Contact;
+
